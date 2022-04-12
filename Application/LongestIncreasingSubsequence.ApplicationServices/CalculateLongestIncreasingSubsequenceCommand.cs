0e8fbd1de0d9832bc7c64a0551d96b2428839f3e
Sugath Bandara <30865958+sugathb@users.noreply.github.com>
2022-04-12 17:01:39 +1000

    implement-calculate-longest-increasing-subsequence-method

--- Application/LongestIncreasingSubsequence.ApplicationServices/CalculateLongestIncreasingSubsequenceCommand.cs
+++ Application/LongestIncreasingSubsequence.ApplicationServices/CalculateLongestIncreasingSubsequenceCommand.cs
@@ -19,7 +19,45 @@ namespace LongestIncreasingSubsequence.ApplicationServices
     {
         public Task<string> Handle(CalculateLongestIncreasingSubsequenceCommand request, CancellationToken cancellationToken)
         {
-            throw new System.NotImplementedException();
+            var longestSequence = CalculateLongestIncreasingSubsequence(request.Numbers);
+            return Task.FromResult(string.Join(" ", longestSequence));
+        }
+
+        private static IEnumerable<int> CalculateLongestIncreasingSubsequence(IEnumerable<int> numbers)
+        {
+            var currentSequence = new List<int>();
+            var longestSequence = new List<int>();
+            var currentSequenceNumber = 0;
+            var longestSequenceNumber = 0;
+            var previousNumber = int.MinValue;
+
+            foreach (var number in numbers)
+            {
+                if (number > previousNumber)
+                {
+                    currentSequence.Add(number);
+                    currentSequenceNumber += 1;
+                    previousNumber = number;
+                }
+                else
+                {
+                    if (currentSequenceNumber > longestSequenceNumber)
+                    {
+                        longestSequenceNumber = currentSequenceNumber;
+                        longestSequence.Clear();
+                        longestSequence.AddRange(currentSequence);
+
+                    }
+
+                    currentSequenceNumber = 1;
+                    currentSequence.Clear();
+                    currentSequence.Add(number);
+                    previousNumber = number;
+                }
+            }
+
+            //Include last number for the calculation
+            return currentSequenceNumber > longestSequenceNumber ? currentSequence : longestSequence;
         }
     }
 }
