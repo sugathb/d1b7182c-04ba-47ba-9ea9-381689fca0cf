e3020426a845a29c5d767eab761b94a6e220d11a
Sugath Bandara <30865958+sugathb@users.noreply.github.com>
2022-04-12 19:42:13 +1000

    split-numbers-command

---
+++ Application/LongestIncreasingSubsequence.ApplicationServices/SplitNumbersCommand.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace LongestIncreasingSubsequence.ApplicationServices
+{
+    public class SplitNumbersCommand : IRequest<IEnumerable<int>>
+    {
+        public SplitNumbersCommand(string numbers)
+        {
+            Numbers = numbers;
+        }
+
+        public string Numbers { get; }
+
+    }
+
+    public class SplitNumbersCommandHandler : IRequestHandler<SplitNumbersCommand, IEnumerable<int>>
+    {
+        public Task<IEnumerable<int>> Handle(SplitNumbersCommand request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(request.Numbers.Split(" ").Select(int.Parse));
+        }
+    }
+}
